94f0a31794413d9332d819c822a20a038534101e
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-12-20 13:25:41 +0200

    Fix formatting in ThemeProvider component and add DropdownMenu component with various subcomponents

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -7,8 +7,7 @@ export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return
-   <NextThemesProvider 
-   {...props}>{children}
-   </NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  );
 }
