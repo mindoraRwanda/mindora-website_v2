1512dd1b7c2c799a4e657dece7828fdfabee5853
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2024-12-20 15:48:42 +0200

    Add TopSection component to About page for enhanced content presentation

--- src/app/(md)/about/page.tsx
+++ src/app/(md)/about/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import TopSection from "@/components/TopSection";
 
 export default function About () {
   return (
-    <div>About page</div>
+    <>
+     <TopSection
+        backgroundImage="/images/aboutbg.jpg"
+        title="About Us"
+        description="Discover the story behind Mindora Africa and our mission to innovate and inspire."
+      />
+    About page
+    </>
   )
 }
