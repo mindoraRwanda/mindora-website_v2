94de8825a7c614ee8605b4e4c84a4dda6996070b
Kevin <carterk@gmail.comgit config --global --listgit config --global user.email carterk@gmail.com>
2025-02-04 01:59:03 +0200

    Add middleware for authentication on /ds/ routes

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+import { NextRequest } from "next/server";
+
+export default withAuth(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    async function middleware(request: NextRequest) {
+        //console.log("middleware", request.auth);
+    }, {
+        isReturnToCurrentPage: true,
+    }
+)
+
+export const config = {
+    matcher: [
+        // Only protect routes starting with /ds/
+        '/(ds)/:path*'
+    ]
+}
